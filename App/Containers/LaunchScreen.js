d8693d06af709b62f78b309a43694ba3207281e1
Brandon Kleiman <brandonwkleiman@gmail.com>
2017-04-01 11:33:15 -0700

    Initial commit.

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -7,14 +7,6 @@ import styles from './Styles/LaunchScreenStyles'
 
 export default class LaunchScreen extends React.Component {
 
-  openRunTracker = () => {
-    this.props.navigation.navigate('RunTracker')
-  }
-  openProfile = () => {
-    this.props.navigation.navigate('Profile')
-  }
-
-
 
   render () {
     return (
@@ -24,11 +16,11 @@ export default class LaunchScreen extends React.Component {
           <View style={styles.centered}>
             <Image source={Images.launch} style={styles.logo} />
           </View>
-          
             <View style={styles.buttonsContainer}>
               <ButtonBox onPress={this.openRunTracker} style={styles.componentButton} image={Images.chevronRight} text="Let's Run" />
               <ButtonBox onPress={this.openProfile} style={styles.usageButton} image={Images.components} text='View Profile' />
             </View>
+
 
         </ScrollView>
       </View>
